fdc07e063009e500491ea4b5a3d0f7ca44b7e16e
Rell2005 <tdrane85@gmail.com>
2025-01-26 19:02:26 +0000

    ive done what i could

--- main.py
+++ main.py
@@ -4,8 +4,37 @@
 # Problem 1
 # Put your solution here, make sure I can run it by running this file. Do not submit it commented out.
 
+def my_list(): 
+ my_list(1 , 5, 'apple', 20.5)
+ print('apple')
+ my_list.append(10)
+ print(my_list)
+ my_list.remove(20.5)
+ print(my_list)
+ my_list.reverse
+ print(my_list)
+
 # Problem 2
 # Put your solution here, make sure I can run it by running this file. Do not submit it commented out.
+
+def dict (keys):(get_items)
+ keys = ('name' , 'age', 'job')
+ get_items = ('john', '30', 'teacher')
+ print('job')
+ dict.appendkeys('city')
+ dict.apppendget_items('paris')
+ print[keys : get_items]
+ dict.removekey("age")
+ dict.removeget_items("30")
+ print[ keys : get_items]
+ keys == get_items
+ print('name')
+ print('job')
+ print('city')
+ print('john')
+ print('teacher')
+ print('paris')
+
 
 # -----------------------------------------------------------------------------
 
@@ -38,6 +67,23 @@ def count_vowels(s: str) -> int:
 
 
 # Unit Tests for count_vowels
+
+def test_count_vowels():
+    test(count_vowels("a") == 1)
+    test(count_vowels("e") == 2)
+    test(count_vowels("i")==3)
+    test(count_vowels("o")==4)
+    test(count_vowels("u")==5)
+    test(count_vowels("y")==0)
+    test(count_vowels("aeiou") == 5)
+    test(count_vowels("") == 0)
+    test(count_vowels("bcdfg") == 0)
+    test(count_vowels("aeiouAEIOU") == 10)
+    test(count_vowels("A") == 2)
+    test(count_vowels("aEiOu") == 5)
+    test(count_vowels("a e i o u") == 5)
+    test(count_vowels("rhythm") == 0)
+
 def test_count_vowels():
     test(count_vowels("hello") == 2)
     test(count_vowels("why") == 0)
@@ -66,6 +112,19 @@ def merge_lists(list1: list, list2: list) -> list:
     # TODO: Implement this function
     pass
 
+def test_merge_lists():
+    list1 = [1, 3, 5]
+    list2 = [2, 4, 6]
+    merged = merge_lists(list1, list2)
+    test(merged == [1, 2, 3, 4, 5, 6])
+    test(merge_lists([2], [2]) == [2,2])
+    test(merge_lists([1], [2]) == [1, 2])
+    test(merge_lists([1, 1], [2, 2]) == [1, 1, 2, 2])
+    test(merge_lists([1, 3, 5], [1, 3, 5]) == [1, 3, 5])
+    test(merge_lists([2, 4, 6], [2, 4, 6]) == [2, 4, 6])
+    test(merge_lists([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6])
+    test(merge_lists([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    test(merge_lists([1, 1, 2, 3], [1, 2, 2, 3]) == [1, 1, 1, 2, 2, 2, 3, 3])
 
 # Unit Tests for merge_lists
 def test_merge_lists():
@@ -97,6 +156,18 @@ def word_lengths(words: list) -> list:
     # TODO: Implement this function
     pass
 
+def test_word_lengths():
+    words = ["Hey", "My", "python", "world", "word"]
+    lengths = word_lengths(words)
+    test(lengths == [5, 5, 6])
+    test(word_lengths(["My"]) == [2])
+    test(word_lengths(["Hey"]) == [3])
+    test(word_lengths(["python"]) == [6])
+    test(word_lengths(["world"]) == [5])
+    test(word_lengths(["word"]) == [4])
+    test(word_lengths(["short", "mediummm", "longesttttt"]) == [5, 8, 10])
+    test(word_lengths(["", "a", "ab", "abc"]) == [0, 1, 2, 3])
+    test(word_lengths(["  ", "a b", " c "]) == [2, 3, 3])
 
 # Unit Tests for word_lengths
 def test_word_lengths():
@@ -124,6 +195,17 @@ def reverse_string(s: str) -> str:
     # TODO: Implement this function
     pass
 
+
+def test_reverse_string():
+    text = "python"
+    reversed_text = reverse_string(text)
+    test(reversed_text == "nohtyp")
+    test(reverse_string("") == "")
+    test(reverse_string("a") == "a")
+    test(reverse_string("aaa!") == "!aaa")
+    test(reverse_string("goodbye, World!") == "!dlroW ,eybdoog")
+    test(reverse_string("7711") == "1177")
+    test(reverse_string("  taste  ") == "  etsat  ")
 
 # Unit Tests for reverse_string
 def test_reverse_string():
@@ -153,6 +235,19 @@ def intersection(list1: list, list2: list) -> list:
     # TODO: Implement this function
     pass
 
+
+def test_intersection():
+    list1 = [1, 2, 3, 4]
+    list2 = [3, 4, 5, 6]
+    result = intersection(list1, list2)
+    test(result ==  [])
+    test(intersection([], []) == [])
+    test(intersection([1], [1]) == [1])
+    test(intersection([1, 2], [3, 4]) == [])
+    test(intersection([1, 2], [1, 2]) == [1, 2])
+    test(intersection([1, 2, 2, 3], [2, 2, 3, 4]) == [2, 3])
+    test(intersection([5, 2, 3], [4, 5, 6]) == [5])
+    test(intersection([1, 2, 3], [1, 2, 3]) == [1, 2, 3])
 
 # Unit Tests for intersection
 def test_intersection():
